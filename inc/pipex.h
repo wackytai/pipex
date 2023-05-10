840d528e526257386d4797746bc6eac0c79d6824
wackytai <tainahmonteiro.info@gmail.com>
2023-05-10 14:44:27 +0100

    Start Handling Inputs

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:07:14 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/05/10 13:13:08 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:16:26 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+typedef struct s_fd
+{
+	int	infile;
+	int	outfile;
+}	t_fd;
+
 /* main.c */
-int		main(void);
-void	writestr(int fd, const char *str);
+int		main(int argc, char **argv);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,55 +6,36 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:03:30 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/05/10 13:13:02 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:34:27 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
-void	writestr(int fd, const char *str)
+int	main(int argc, char **argv)
 {
-	write(fd, str, strlen(str));
-}
+	t_fd	fds;
 
-int	main(void)
-{
-	int		pipefd[2];
-	pid_t	pid;
-	char	buf;
-
-	if (pipe(pipefd) == -1)
+	if (argc >= 5 && argv)
 	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		close(pipefd[1]);
-		writestr(STDOUT_FILENO, "Child: What is the secret in this pipe?\n");
-		writestr(STDOUT_FILENO, "Child: \"");
-		while (read(pipefd[0], &buf, 1) > 0)
-			write(STDOUT_FILENO, &buf, 1);
-		writestr(STDOUT_FILENO, "\"\n");
-		writestr(STDOUT_FILENO, "Child: Wow! I must go see my father.\n");
-		close(pipefd[0]);
-		exit(EXIT_SUCCESS);
+		printf("N args: OK\n");
+		if (access(argv[1], R_OK) == 0)
+		{
+			printf("infile access: OK\n");
+			fds.infile = open(argv[1], O_RDONLY);
+			printf("infile fd: %i\n", fds.infile);
+		}
+		else
+			printf("infile access: KO\n");
+		if (access(argv[argc - 1], W_OK) == 0)
+			printf("outfile access: OK\n");
+		else
+			printf("outfile access: KO\n");
 	}
 	else
 	{
-		close(pipefd[0]);
-		writestr(STDOUT_FILENO, "Parent: I'm writing a secret in this pipe...\n");
-		writestr(pipefd[1], "\e[33mI am your father mwahahahahah!\e[0m");
-		close(pipefd[1]);
-		wait(NULL);
-		writestr(STDOUT_FILENO, "Parent: Hello child!\n");
-		exit(EXIT_SUCCESS);
+		printf("number of arguments is wrong\n");
+		return (0);
 	}
 	return (0);
 }
