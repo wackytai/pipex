6e028500505e7d4522a4b42c6020577277431897
wackytai <tainahmonteiro.info@gmail.com>
2023-06-06 11:25:41 +0100

    here_doc working

--- src/bonus/pipex_bonus.c
+++ src/bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:03:30 by tlemos-m          #+#    #+#             */
-/*   Updated: 2023/06/06 10:34:41 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/06/06 11:22:46 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,11 @@ int	main(int argc, char **argv, char **envp)
 	fds.n_cmds = argc - 3 - fds.ishdoc;
 	if (check_outfile(argc, argv, &fds) != 0)
 		return (file_error(fds));
-	here_doc(&fds, argv[2]);
-	/* create_pipes(&fds);
+	if (envp)
+		here_doc(&fds, argv[2]);
+	create_pipes(&fds);
 	fds.paths = get_paths(envp);
-	create_process(argv, &fds, envp); */
+	create_process(argv, &fds, envp);
 	if (fds.paths != 0)
 		free_array(fds.paths);
 	close_files(fds);
@@ -55,7 +56,7 @@ int	check_infile(char *argv, t_fd *fds)
 	if (ft_strncmp(argv, "here_doc", ft_strlen(argv)) == 0)
 	{
 		fds->ishdoc = 1;
-		fds->infile = open(argv, O_RDWR | O_CREAT | O_APPEND, 0644);
+		fds->infile = open(argv, O_RDWR | O_CREAT | O_TRUNC, 0644);
 		return (0);
 	}
 	if (access(argv, F_OK | R_OK) != 0)
@@ -87,29 +88,18 @@ int	create_process(char **argv, t_fd *fds, char **envp)
 
 int	here_doc(t_fd *fds, char *limiter)
 {
-	int		fd[2];
-	int		pid;
 	char	*line;
 
-	if (pipe(fd) != 0)
-		process_error(0);
-	pid = fork();
-	if (pid < 0)
-		process_error(1);
-	if (pid == 0)
+	while (1)
 	{
-		close(fd[0]);
-		dup2(fds->infile, STDOUT_FILENO);
-		while (ft_strncmp(line, limiter, ft_strlen(limiter)) != 0)
-			line = get_next_line(STDIN_FILENO);
-		ft_putstr_fd(line, fd[1]);
+		write(STDOUT_FILENO, "pipex: ", 7);
+		line = get_next_line(STDIN_FILENO);
+		if (!line || (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0
+				&& line[ft_strlen(limiter)] == '\n'))
+			break ;
+		ft_putstr_fd(line, fds->infile);
 		free(line);
 	}
-	else
-	{
-		close(fd[1]);
-		close(fd[0]);
-		waitpid(pid, NULL, 0);
-	}
+	free(line);
 	return (0);
 }
