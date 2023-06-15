283d927c31106af3cd5f3cc186910ca2002d5256
wackytai <tainahmonteiro.info@gmail.com>
2023-06-15 20:45:18 +0100

    ft_printf() placed

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 15:53:27 by tlemos-m          #+#    #+#             */
-/*   Updated: 2022/11/23 10:46:52 by tlemos-m         ###   ########.fr       */
+/*   Updated: 2023/06/15 20:29:39 by tlemos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	ft_putchar(char c);
 int		ft_putstr(char *s);
 int		ft_putnbr_base(long long n, char *base, int b);
 int		ft_putptr_base(unsigned long n, char *base, int j);
-size_t	ft_strlen(char *s);
+size_t	ft_strlen(const char *s);
 
 #endif
